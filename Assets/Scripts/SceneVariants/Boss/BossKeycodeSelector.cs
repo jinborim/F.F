497160ba0ec16ce7fdec02daee2b83074cb8a2ef
jinborim <jj8638638@naver.com>
2026-09-06 16:08:59 +0900

    확인된 씬 오브젝트에 스크립트 연결

---
+++ Assets/Scripts/SceneVariants/Boss/BossKeycodeSelector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKeycodeSelector : MonoBehaviour
+{
+    public KeyCode[] keyCodes = {
+    KeyCode.Alpha1,
+    KeyCode.Alpha2,
+    KeyCode.Alpha3,
+    KeyCode.Alpha4,
+    KeyCode.Alpha5,
+    KeyCode.Alpha6,
+    KeyCode.Alpha7,
+    KeyCode.Alpha8,
+    KeyCode.Alpha9,
+};
+
+ 
+}
